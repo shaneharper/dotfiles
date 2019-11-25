91a9f7d1d4c46418b8bc1a071e87de2fd25a8510
Shane Harper <shane@shaneharper.net>
2019-11-26 09:20:19 +1100

    .
    No longer require the absolute path for hgignore be ~/dotfiles/hgignore.
    
    (I intend that the absolute path for hgignore will be ~/win_home/dotfiles/hgignore when I access this repository from WSL/Ubuntu.)

--- install.bat
+++ install.bat
@@ -4,6 +4,9 @@
 echo source %script_directory%vimrc >> %USERPROFILE%\.vimrc
 git config --global core.excludesfile %script_directory%/gitignore
 git config --global push.default simple
+
+echo [ui]>> %USERPROFILE%\.hgrc
+echo ignore = %script_directory%hgignore>> %USERPROFILE%\.hgrc
 echo %%include %script_directory%hgrc>> %USERPROFILE%\.hgrc
 
 REM copy ctags.cnf %userprofile%
