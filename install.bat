fa7682dcfd8e6e2b904078e0368a04a83d2f387b
Shane Harper <shane@shaneharper.net>
2019-11-26 08:19:22 +1100

    .
    (trivial) remove trailing space written to .hgrc.

--- install.bat
+++ install.bat
@@ -4,6 +4,6 @@
 echo source %script_directory%vimrc >> %USERPROFILE%\.vimrc
 git config --global core.excludesfile %script_directory%/gitignore
 git config --global push.default simple
-echo %%include %script_directory%hgrc >> %USERPROFILE%\.hgrc
+echo %%include %script_directory%hgrc>> %USERPROFILE%\.hgrc
 
 REM copy ctags.cnf %userprofile%
