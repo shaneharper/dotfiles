24d48d79ead33f4828409f75bda1da89bea24075
Shane Harper <shane@shaneharper.net>
2023-06-14 12:36:52 +1000

    Rename windows_terminal_settings.json -> windows_terminal_settings--i7_12700K_PC.json.
    The file, as is, works on my PC. Some changes are required to use the file on my XPS 15 laptop; Different guids are used for the Visual Studio profiles (see comments at end of install.bat).

--- install.bat
+++ install.bat
@@ -11,4 +11,9 @@ echo [ui]>> %USERPROFILE%\.hgrc
 echo ignore = %script_directory%hgignore>> %USERPROFILE%\.hgrc
 echo %%include %script_directory%hgrc>> %USERPROFILE%\.hgrc
 
-REM copy windows_terminal_settings.json %USERPROFILE%\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
+REM Settings for Windows Terminal
+REM  - Backup %USERPROFILE%\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json.
+REM  - copy windows_terminal_settings--i7_12700K_PC.json %USERPROFILE%\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
+REM  - The following profile guids are used on my XPS 15 laptop:
+REM      "x64 Developer Command Prompt for VS 2022": {d3d858ea-2ed2-5953-8629-0b7f3fc358ee}
+REM      "Developer PowerShell for VS 2022": {1b924807-e994-54ba-9f4e-8d9cd916ba97}
