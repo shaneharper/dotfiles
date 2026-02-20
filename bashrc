shaneharper/dotfiles

commit 125a59200c5c05e0ec6f09a2ca9a2afa6794693d

gd, gsh, hgd and hge functions: No longer display "Vim: Reading from stdin...".

bashrc
@@ -32,15 +32,18 @@ pipe_if_not_empty()  # Initially copied from https://superuser.com/a/210141.
     fi
 }
 
-# xxx The following functions should return the return code of the first command ("git diff", "hg diff", etc.) when that command fails. vimless shouldn't be run if the first command fails - It seems that that is what normally happens anyway; in the case of the first command failing usually nothing is output to stdout and consequently pipe_if_not_empty() won't run vimless.
-gd() { git diff "$@" | clean_up_vc_diff | pipe_if_not_empty vimless; }
-gsh() { git show "$@" | clean_up_vc_diff | pipe_if_not_empty vimless; }
-hgd() { hg diff "$@" | clean_up_vc_diff | pipe_if_not_empty vimless; }
+__pager() { pipe_if_not_empty vimless --not-a-term -; }  # (--not-a-term stops Vim displaying "Vim: Reading from stdin...".)
+
+
+# xxx The following functions should return immediately with the return code of the first command ("git diff", "hg diff", etc.) if that command fails. (Presently if the first command fails without outputting anything to stdout then __pager will return immediately and anything written to stderr will immediately be visible.)
+gd() { git diff "$@" | clean_up_vc_diff | __pager; }
+gsh() { git show "$@" | clean_up_vc_diff | __pager; }
+hgd() { hg diff "$@" | clean_up_vc_diff | __pager; }
 hge() { hg export --template "commit {node}{ifeq(branch, 'default', '', '  {branch}')}
 {date|rfc822date}{ifeq(author|email, 'shane@shaneharper.net', '', '  {author|email}')}
 {indent(desc, '    ')}
 
-{diff}" "$@" | clean_up_vc_diff | pipe_if_not_empty vimless; }
+{diff}" "$@" | clean_up_vc_diff | __pager; } 
 
 # ------------------------------------------------------------------------ }}}
 

commit aeb23c4e495799a37fd795c50183f8dc20a4524f

bashrc: Fix piping output from gd and hgd broken in 125a59200c.
Note that since Vim 515da6ecdb (just before v9.2.0017) Vim's runtime/macros/less.sh now passes "--not-a-term" to vim so there's no need for vimless() to do that (to prevent "Vim: Reading from stdin..." from being visible after vim exited).

Passing an argument to less.sh that wasn't a filename was what broke being able to pipe the output of gd and hgd as less.sh would pass the argument to cat in such a case.

bashrc
@@ -45,7 +45,7 @@ pipe_if_not_empty()  # Initially copied from https://superuser.com/a/210141.
     fi
 }
 
-__pager() { pipe_if_not_empty vimless --not-a-term -; }  # (--not-a-term stops Vim displaying "Vim: Reading from stdin...".)
+__pager() { pipe_if_not_empty vimless; }
 
 
 # xxx The following functions should return immediately with the return code of the first command ("git diff", "hg diff", etc.) if that command fails. (Presently if the first command fails without outputting anything to stdout then __pager will return immediately and anything written to stderr will immediately be visible.)